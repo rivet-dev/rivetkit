ce8ba4fcf76f840d5619a530c2907856043ff99a
Nathan Flurry <developer@nathanflurry.com>
2025-09-17 00:21:05 -0700

    chore(core): skip sleep tests in memory

--- packages/rivetkit/tests/driver-memory.test.ts
+++ packages/rivetkit/tests/driver-memory.test.ts
@@ -5,6 +5,10 @@ import { createFileSystemOrMemoryDriver } from "@/drivers/file-system/mod";
 runDriverTests({
 	// TODO: Remove this once timer issues are fixed in actor-sleep.ts
 	useRealTimers: true,
+	skip: {
+		// Sleeping not enabled in memory
+		sleep: true,
+	},
 	async start() {
 		return await createTestRuntime(
 			join(__dirname, "../fixtures/driver-test-suite/registry.ts"),
