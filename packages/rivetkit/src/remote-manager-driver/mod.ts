89a9d24670a19178ef279dc93a1c8155278a81e1
Nathan Flurry <developer@nathanflurry.com>
2025-09-25 00:41:06 -0700

    fix(core): fix passing incorrect actor input to create

--- packages/rivetkit/src/remote-manager-driver/mod.ts
+++ packages/rivetkit/src/remote-manager-driver/mod.ts
@@ -139,7 +139,9 @@ export class RemoteManagerDriver implements ManagerDriver {
 			name,
 			key: serializeActorKey(key),
 			runner_name_selector: this.#config.runnerName,
-			input: input ? cbor.encode(actorInput).toString("base64") : undefined,
+			input: actorInput
+				? cbor.encode(actorInput).toString("base64")
+				: undefined,
 			crash_policy: "sleep",
 		});
 
