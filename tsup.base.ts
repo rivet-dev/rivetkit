cda27974d4b838f6a05f20c8b4dc9e47865be3e7
NathanFlurry <git@nathanflurry.com>
2025-10-06 19:41:31 +0000

    chore(core): add `build:fast` script (#1355)

--- tsup.base.ts
+++ tsup.base.ts
@@ -1,17 +1,22 @@
 import type { Options } from "tsup";
 
+const FAST_BUILD = process.env.FAST_BUILD === "1";
+
 export default {
 	target: "node16",
 	platform: "node",
 	format: ["cjs", "esm"],
 	sourcemap: true,
 	clean: true,
-	dts: {
-		compilerOptions: {
-			skipLibCheck: true,
-			resolveJsonModule: true,
-		},
-	},
+	// DTS takes the longest time, so we skip it if doing a fast build
+	dts: FAST_BUILD
+		? false
+		: {
+				compilerOptions: {
+					skipLibCheck: true,
+					resolveJsonModule: true,
+				},
+			},
 	minify: false,
 	// IMPORTANT: Splitting is required to fix a bug with ESM (https://github.com/egoist/tsup/issues/992#issuecomment-1763540165)
 	splitting: true,
