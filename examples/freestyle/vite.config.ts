443e8b0dc44076257137d2615b687a778871f539
abcxff <79597906+abcxff@users.noreply.github.com>
2025-10-09 00:54:35 +0000

    feat: freestyle example (#1317)
    Fixes KIT-302

---
+++ examples/freestyle/vite.config.ts
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	plugins: [react()],
+	root: "src/frontend",
+	build: {
+		outDir: "../../dist/public",
+	},
+	envDir: ".",
+	server: {
+		host: "0.0.0.0",
+		port: 5173,
+	},
+});
